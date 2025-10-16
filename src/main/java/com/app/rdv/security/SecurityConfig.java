297733bb85b1af78882ce88f80ba422ee013fc05
abderrazk <makrem050@live.fr>
2025-10-16 17:01:48 +0100

    role security

--- src/main/java/com/app/rdv/security/SecurityConfig.java
+++ src/main/java/com/app/rdv/security/SecurityConfig.java
@@ -2,16 +2,26 @@ package com.app.rdv.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final HttpSecurity httpSecurity;
+
+    public SecurityConfig(HttpSecurity httpSecurity) {
+        this.httpSecurity = httpSecurity;
+    }
 
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder) {
@@ -31,5 +41,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    public SecurityFilterChain securityFilterChain(SecurityFilterChain securityFilterChain) throws Exception {
+        return httpSecurity
+                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->auth.requestMatchers("/api/rdv/create","/api/medecin/create","/api/patient/add").hasAnyAuthority("Admin")
+                        .requestMatchers("/api/medecin/all").permitAll())
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
